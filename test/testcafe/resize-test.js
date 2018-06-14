a0850dd3ffc8cd5c0ed0399946e53b829211068e
Patryk Kopycinski <contact@patrykkopycinski.com>
2018-06-14 21:36:49 +0200

    Fix tests

--- test/testcafe/resize-test.js
+++ test/testcafe/resize-test.js
@@ -5,6 +5,11 @@ fixture `Resize`
     .page('https://google.com');
 
 test('Resize test', async t => {
+    var originalSize = await t.eval(() => ({
+        width:  window.innerWidth,
+        height: window.innerHeight
+    }));
+
     await t.resizeWindow(500, 500);
 
     var newSize = await t.eval(() => ({
@@ -12,6 +17,6 @@ test('Resize test', async t => {
         height: window.innerHeight
     }));
 
-    expect(newSize.width).to.be.equal(500);
-    expect(newSize.height).to.be.equal(500);
+    expect(newSize.width).to.be.not.equal(originalSize.width);
+    expect(newSize.height).to.be.not.equal(originalSize.height);
 });
